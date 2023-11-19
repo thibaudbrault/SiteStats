a368ca14f869a3b7fa6a22c1061d57754898ccdb
Thibaud Brault <thibaud.brault@protonmail.com>
2023-11-19 17:25:23 +0100

    :construction: create cloudflare component

---
+++ components/Cloudflare.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="section">
+    <h2 class="title" style="font-variant: small-caps">Cloudflare</h2>
+  </section>
+</template>
+<script setup></script>
+<style lang="postcss" scoped>
+.section {
+  .title {
+    @apply font-semibold text-7xl text-orange-600 text-center lowercase pb-8;
+  }
+}
+</style>
