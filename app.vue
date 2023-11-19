2c6bbe7a13e0b72d2f996bbbfe511efa669800a1
Thibaud Brault <thibaud.brault@protonmail.com>
2023-11-19 17:24:43 +0100

    :sparkles: create header with title and nav

--- app.vue
+++ app.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-    <header>
-      <h1 class="title">SiteStats</h1>
+  <div class="app">
+    <UiTooltipProvider>
+    <header class="header">
+      <div class="name">
+        <Icon name="iconoir:network-left" />
+        <h1 class="title">SiteStats</h1>
+      </div>
+      <nav class="nav">
+            <NuxtLink to="#netlify">Netlify</NuxtLink>
+            <NuxtLink to="#github">Github</NuxtLink>
+      </nav>
     </header>
-    <UiButton>Click me</UiButton>
     <NuxtPage />
+  </UiTooltipProvider>
   </div>
 </template>
 
-<style>
-.title {
-  color: red;
+<style scoped lang="postcss">
+.app {
+  @apply bg-background text-foreground min-h-screen p-8 scroll-smooth;
+  .header {
+    @apply pb-8 flex justify-between items-center;
+    .name {
+      @apply flex gap-2 items-center;
+      svg {
+        @apply text-4xl;
+      }
+      .title {
+        @apply font-bold text-foreground text-4xl
+      }
+    }
+    .nav {
+      @apply flex gap-4;
+    }
+  }
 }
 </style>
