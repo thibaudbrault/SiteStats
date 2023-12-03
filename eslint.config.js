f86c20dccf513e819964dbe463723f8a0314c4b1
Thibaud Brault <thibaud.brault@protonmail.com>
2023-12-03 20:40:04 +0100

    :see_no_evil: add file to ignore in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,9 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  ignores: [
+    'tailwind.config.ts',
+    'nuxt.config.ts',
+    'types/*.ts',
+  ],
+})
