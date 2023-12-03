0b8b6ddffed2e8d4b284ee14f04f8425638aa431
Thibaud Brault <thibaud.brault@protonmail.com>
2023-12-03 20:25:57 +0100

    :sparkles: create field component

---
+++ components/ui/field/Field.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="field">
+    <Label :for="value">{{ label }}</Label>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<style scoped lang="postcss">
+.field {
+  @apply flex flex-col gap-2;
+}
+</style>
